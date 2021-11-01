a6654b57f710e99cb55d149d7c89c4b74cc75688
mustafacem <67694833+mustafacem@users.noreply.github.com>
2021-11-02 00:18:42 +0300

    Add files via upload

---
+++ scr_kılıc.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_kılıc : MonoBehaviour {
+
+    public float savurma_hızı = 2f;
+    public float soguma_hızı = 2f;
+    public float soguma_suresi = 0.5f;
+    public float saldırı_suresi = 0.35f;
+
+    private Quaternion hedef_pozisyon;
+    private float sogma_alarmı;
+    private bool saldırıyor;
+	// Use this for initialization
+	void Start () {
+        hedef_pozisyon = Quaternion.Euler(0,0,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, hedef_pozisyon, Time.deltaTime * (saldırıyor ? savurma_hızı : soguma_hızı ));//local olunca asıl obje(oyuncu )rotasyonu değişse bile sıfır kaliyor //saldırıyor kompkat if işste anala
+
+        sogma_alarmı -= Time.deltaTime;
+
+    }
+
+    public void Saldırı()
+    {
+        if (sogma_alarmı>0f)
+        {
+            return; 
+        }
+        hedef_pozisyon = Quaternion.Euler(-90, 0, 0);
+
+        sogma_alarmı = soguma_suresi;
+        StartCoroutine(Bekleme_soguma());
+    }
+   private IEnumerator Bekleme_soguma()//bekleme için alarımın parçası
+    {
+        saldırıyor = true;
+       yield return new WaitForSeconds(saldırı_suresi);
+        saldırıyor = false;
+       hedef_pozisyon = Quaternion.Euler(0, 0, 0);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+      //  GameObject.addfor(, transform.position, transform.rotation);
+
+      //  Destroy(gameObject);
+    }
+
+}
---
+++ scr_oyuncu.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_oyuncu : MonoBehaviour {
+    //public float zıp_guc;
+    [Header("gorsel")]
+    public GameObject modeller;
+    public float donme_hız;
+
+    [Header("hareket")]
+
+    public float zıp_surat;
+    public float hareket_surat;
+
+    [Header("ekipman")]
+    public  scr_kılıc kılıc;//once genel  scriptte spnra ozel lismiş 
+
+
+    private bool zıp_izin;
+    private Rigidbody oyuncu_rigidbody;
+    private Quaternion model_yon;
+	// Use this for initialization
+	void Start () {
+        oyuncu_rigidbody = GetComponent<Rigidbody>();
+        model_yon = Quaternion.Euler(0, 0, 0);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+      
+        RaycastHit vurdu;
+        if (Physics.Raycast(transform.position, Vector3.down, out vurdu, 1.01f))//altan bir şeyye ışın değiyorsa
+        {
+            zıp_izin = true;
+
+        }
+       // modeller.transform.rotation = Quaternion.Lerp(modeller.transform.rotation, model_yon, Time.deltaTime * donme_hız);
+
+       ProcessInput();
+    }
+
+
+    void ProcessInput()
+    {
+
+
+        oyuncu_rigidbody.velocity = new Vector3(
+                0,
+                 oyuncu_rigidbody.velocity.y,
+                0);
+       
+        if (zıp_izin == true && Input.GetKeyDown("space"))
+        {
+            zıp_izin = false;
+            // GetComponent<Rigidbody>().AddForce(0, zıp_guc, 0);
+            oyuncu_rigidbody.velocity = new Vector3(oyuncu_rigidbody.velocity.x, zıp_surat, oyuncu_rigidbody.velocity.z);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+           kılıc.Saldırı();
+        }
+
+    }
+
+
+}
