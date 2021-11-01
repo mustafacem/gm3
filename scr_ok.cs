dec2ebbc6122177a308569c016e5c727027b744c
mustafacem <67694833+mustafacem@users.noreply.github.com>
2021-11-02 00:22:25 +0300

    Add files via upload

---
+++ scr_ok.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_ok : MonoBehaviour
+{
+    Rigidbody ok_kendisi;
+    private float lifeTimer = 2f;
+    private float timer;
+    private bool vurdumu= false;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        ok_kendisi= GetComponent<Rigidbody>();
+        transform.rotation = Quaternion.LookRotation(ok_kendisi.velocity);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= lifeTimer)
+        {
+            Destroy(gameObject);
+        }
+
+        if (!vurdumu)
+        {
+            transform.rotation = Quaternion.LookRotation(ok_kendisi.velocity);
+        }
+
+       
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag != "ok")
+        {
+            vurdumu = true;
+            yapıs();
+        }
+    }
+
+    private void yapıs()
+    {
+        ok_kendisi.constraints = RigidbodyConstraints.FreezeAll;
+    }
+
+}
---
+++ scr_yay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_yay : MonoBehaviour
+{
+    public Camera kamera;
+    public GameObject Ok;
+    public Transform ok_cıkıs/*senin eski emitter*/;
+    public float ok_hızı;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            GameObject go = Instantiate(Ok, ok_cıkıs.position, Quaternion.identity);
+            Rigidbody rb = go.GetComponent<Rigidbody>();
+            rb.velocity = kamera.transform.forward * ok_hızı; 
+        }
+        
+    }
+}
